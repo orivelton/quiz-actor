77ba5f8d78f4bba2d0b7aea1d3342e18d81423fa
Orivelton Cesar da Conceição <orivelton.cesar@keyrus.com.br>
2017-09-27 11:52:48 -0300

    start new compoment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ScoreComponent } from './score/score.component';
+import { ImageComponent } from './image/image.component';
+import { QuestionComponent } from './question/question.component';
+import { NextQuestionComponent } from './next-question/next-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ScoreComponent,
+    ImageComponent,
+    QuestionComponent,
+    NextQuestionComponent
   ],
   imports: [
     BrowserModule
